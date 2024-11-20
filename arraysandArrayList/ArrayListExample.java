b170363e2cd647a5a0dc3cc44fa157bbca0cbba2
Sucith <sucith712@gmail.com>
2024-11-20 23:06:31 +0530

    make comments

--- arraysandArrayList/ArrayListExample.java
+++ arraysandArrayList/ArrayListExample.java
@@ -8,10 +8,10 @@ public class ArrayListExample {
 //		ArrayList <Integer> list = new ArrayList<>();  // Syntax if Arraylist
 		
 		ArrayList<Integer> list=new ArrayList<>(5);
-		list.add(2);
-		list.add(4);
-		list.add(6);
-		System.out.println(list);
+//		list.add(2);
+//		list.add(4);
+//		list.add(6);
+//		System.out.println(list);
 //		
 		System.out.println("User Input");
 		Scanner in = new Scanner(System.in);
